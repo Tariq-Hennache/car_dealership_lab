32edd75b374d23d5225a8671f085aca06cdba7d4
Tariq-Hennache <tariq.hennache@gmail.com>
2022-09-22 14:32:07 +0100

    constructors added

---
+++ Dealership.js
@@ -0,0 +1,5 @@
+const Dealership = function( nameOfDealership, maxNoOfCars, carStock){
+    this.nameOfDealership = nameOfDealership;
+    this.maxNoOfCars = maxNoOfCars;
+    this.carStock = carStock;
+}
